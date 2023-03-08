f418da5dff4e79ca2418a89ee9b71f04e92b3ff5
Islam <isxmad@gmail.com>
2023-03-09 01:45:14 +0300

    page load progressbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,20 @@
 import 'swiper/css';
 import '@/styles/styles.scss';
 import type { AppProps } from 'next/app'
-import { useRouter } from 'next/router';
+import { useRouter, Router } from 'next/router';
 import { useEffect } from 'react';
 import Sidebar from '@/components/Sidebar/Sidebar';
 import ContentLayout from '@/components/ContentLayout/ContentLayout';
+import * as NProgress from 'nprogress';
+import 'nprogress/nprogress.css'; 
+
+
+NProgress.configure({ showSpinner: false })
+Router.events.on('routeChangeStart', () => NProgress.start()); 
+Router.events.on('routeChangeComplete', () => NProgress.done()); 
+Router.events.on('routeChangeError', () => NProgress.done());
+
+
 
 export default function App({ Component, pageProps }: AppProps) {
   const {pathname} = useRouter()
