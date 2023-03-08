a75f58e019bdcb36bd03267a6ba5b4d48bc5041d
Islam <isxmad@gmail.com>
2023-03-09 01:12:43 +0300

    test with index & redirect

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,16 @@
 import { useRouter } from "next/router";
 import Router from "next/router";
-
+import ContentLayout from "@/components/ContentLayout/ContentLayout";
 const HomePage = () => {
     Router.push('/console/my-bots')
-    return null;
+    // return null;
+    return (
+        <ContentLayout
+            head=""
+            >
+            {/* <h1>Home</h1> */}
+        </ContentLayout>
+    )
 }
 
 export default HomePage;
