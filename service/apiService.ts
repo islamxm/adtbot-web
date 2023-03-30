614debf98033b1da83019c2d064ff8070edaf8f4
Islam <isxmad@gmail.com>
2023-03-31 02:13:16 +0300

    api methods

--- service/apiService.ts
+++ service/apiService.ts
@@ -1,8 +1,13 @@
 import endpoints from "./endpoints";
 import axios from "axios";
 import checkAuth from "./checkAuth";
+const headers = {
+    'Content-type': 'application/json',
+    'Accept': 'application/json',
+}
 
 class ApiService {
+
     register = async (body: {
         email: string,
         username: string,
@@ -10,7 +15,330 @@ class ApiService {
         is_superuser: false
     }) => {
         try {
-            let res = await axios.post(endpoints.register, body)
+            let res = await fetch(endpoints.register, {
+                method: 'POST',
+                body: JSON.stringify(body),
+                headers,
+                mode: 'no-cors'
+            })
+            return await checkAuth(res)
+        } catch(err) {
+            console.log(err)
+        }
+    }
+
+    verifyUser = async (verify_code: string) => {
+        try {
+            let res = await fetch(endpoints.verifyUser + `?verify_code=${verify_code}`, {
+                method: 'POST',
+                headers,
+                mode: 'no-cors'
+            })
+            return await checkAuth(res)
+        } catch(err) {
+            console.log(err)
+        }
+    }
+
+    verifyIp = async (code: string) => {
+        try {
+            let res = await fetch(endpoints.verifyIp + `?code=${code}`, {
+                method: 'POST',
+                headers,
+                mode: 'no-cors'
+            })
+            return await checkAuth(res)
+        } catch(err) {
+            console.log(err)
+        }
+    }
+
+    editUserData = async (body: {
+        email: string,
+        username: string,
+        password: string,
+        old_password: string
+    }) => {
+        try {
+            let res = await fetch(endpoints.editUserData, {
+                method: 'POST',
+                headers,
+                body: JSON.stringify(body),
+                mode: 'no-cors'
+            })
+            return await checkAuth(res)
+        } catch(err) {
+            console.log(err)
+        }
+    }
+
+    forgotPassword = async (body: {email: string}) => {
+        try {
+            let res = await fetch(endpoints.forgotPassword, {
+                method: 'POST',
+                headers,
+                body: JSON.stringify(body),
+                mode: 'no-cors'
+            })
+            return await checkAuth(res)
+        } catch(err) {
+            console.log(err)
+        }
+    }
+
+    resetPassword = async (code: string, password: string) => {
+        try {
+            let res = await fetch(endpoints.resetPassword + `?code=${code}&password=${password}`, {
+                method: 'POST',
+                headers,
+                mode: 'no-cors'
+            })
+            return await checkAuth(res)
+        } catch(err) {
+            console.log(err)
+        }
+    }
+
+    getOAuth2Token = async (body: {
+        grant_type?: string,
+        username?: string,
+        password?: string,
+        scope?: string,
+        totp_code?: string,
+    }) => {
+        try {
+            let res = await fetch(endpoints.getOAuth2Token, {
+                method: 'POST',
+                headers,
+                body: JSON.stringify(body),
+                mode: 'no-cors'
+            })
+            return await checkAuth(res)
+        } catch(err) {
+            console.log(err)
+        }
+    }
+
+    setTgKey = async (text: string) => {
+        try {
+            let res = await fetch(endpoints.setTgKey + `?text=${text}`, {
+                method: 'POST',
+                headers,
+                mode: 'no-cors'
+            })
+            return await checkAuth(res)
+        } catch(err) {
+            console.log(err)
+        }
+    }
+
+    getTgKey = async () => {
+        try {
+            let res = await fetch(endpoints.getTgKey, {
+                method: 'POST',
+                headers,
+                mode: 'no-cors'
+            })
+            return await checkAuth(res)
+        } catch(err) {
+            console.log(err)
+        }
+    }
+
+
+    addFeedback = async (text: string) => {
+        try {
+            let res = await fetch(endpoints.addFeedback + `?text=${text}`, {
+                method: 'POST',
+                headers,
+                mode: 'no-cors'
+            })
+            return await checkAuth(res)
+        } catch(err) {
+            console.log(err)
+        }
+    }
+
+    getFeedbacks = async (limit: number, offset: string) => {
+        try {
+            let res = await fetch(endpoints.getFeedbacks + `?limit=${limit}&offset=${offset}`, {
+                method: 'POST',
+                headers,
+                mode: 'no-cors'
+            })
+            return await checkAuth(res)
+        } catch(err) {
+            console.log(err)
+        }
+    }
+
+    setTgNot = async (status: boolean) => {
+        try {
+            let res = await fetch(endpoints.setTgNot + `?status=${status}`, {
+                method: 'POST',
+                headers,
+                mode: 'no-cors'
+            })
+            return await checkAuth(res)
+        } catch(err) {
+            console.log(err)
+        }
+    }
+
+    setEmailNot = async (status: boolean) => {
+        try {
+            let res = await fetch(endpoints.setEmailNot + `?status=${status}`, {
+                method: 'POST',
+                headers,
+                mode: 'no-cors'
+            })
+            return await checkAuth(res)
+        } catch(err) {
+            console.log(err)
+        }
+    }
+
+    getNotStatus = async () => {
+        try {
+            let res = await fetch(endpoints.getNotStatus, {
+                method: 'POST',
+                headers,
+                mode: 'no-cors'
+            })
+            return await checkAuth(res)
+        } catch(err) {
+            console.log(err)
+        }
+    }
+
+
+    setGateAuthData = async (body: {
+        apiKey: string,
+        secret: string,
+        password: string
+    }) => {
+        try {
+            let res = await fetch(endpoints.setGateAuthData, {
+                method: 'POST',
+                headers,
+                body: JSON.stringify(body),
+                mode: 'no-cors'
+            })
+            return await checkAuth(res)
+        } catch(err) {
+            console.log(err)
+        }
+    }
+
+    setKucoinAuthData = async (body: {
+        apiKey: string,
+        secret: string,
+        password: string
+    }) => {
+        try {
+            let res = await fetch(endpoints.setKucoinAuthData, {
+                method: 'POST',
+                headers,
+                body: JSON.stringify(body),
+                mode: 'no-cors'
+            })
+            return await checkAuth(res)
+        } catch(err) {
+            console.log(err)
+        }
+    }
+
+    setMexcAuthData = async (body: {
+        apiKey: string,
+        secret: string,
+        password: string
+    }) => {
+        try {
+            let res = await fetch(endpoints.setMexcAuthData, {
+                method: 'POST',
+                headers,
+                body: JSON.stringify(body),
+                mode: 'no-cors'
+            })
+            return await checkAuth(res)
+        } catch(err) {
+            console.log(err)
+        }
+    }
+
+    setHuobiAuthData = async (body: {
+        apiKey: string,
+        secret: string,
+        password: string
+    }) => {
+        try {
+            let res = await fetch(endpoints.setHuobiAuthData, {
+                method: 'POST',
+                headers,
+                body: JSON.stringify(body),
+                mode: 'no-cors'
+            })
+            return await checkAuth(res)
+        } catch(err) {
+            console.log(err)
+        }
+    }
+
+    getExchangeBalances = async () => {
+        try {
+            let res = await fetch(endpoints.getExchangeBalances, {
+                method: 'POST',
+                headers,
+                mode: 'no-cors'
+            })
+            return await checkAuth(res)
+        } catch(err) {
+            console.log(err)
+        }
+    }
+
+    createBot = async (body: {
+        monitor: number,
+        exchange: number,
+        budget_usdt: number,
+        take_profit: number,
+        stop_loss: number,
+        stop_buy: number
+    }) => {
+        try {
+            let res = await fetch(endpoints.createBot, {
+                method: 'POST',
+                headers,
+                body: JSON.stringify(body),
+                mode: 'no-cors'
+            })
+            return await checkAuth(res)
+        } catch(err) {
+            console.log(err)
+        }
+    }
+
+
+    getBots = async () => {
+        try {
+            let res = await fetch(endpoints.getBots, {
+                method: 'POST',
+                headers,
+                mode: 'no-cors'
+            })
+            return await checkAuth(res)
+        } catch(err) {
+            console.log(err)
+        }
+    }
+
+    announceStats = async (first_date: Date, second_date: Date) => {
+        try {
+            let res = await fetch(endpoints.announceStats + `?first_date${first_date}&second_date=${second_date}`, {
+                method: 'POST',
+                headers,
+                mode: 'no-cors'
+            })
             return await checkAuth(res)
         } catch(err) {
             console.log(err)
--- service/endpoints.ts
+++ service/endpoints.ts
@@ -4,6 +4,37 @@ export const PATH = `${BASE_DOMAIN}/api/v1/`
 const endpoints = {
     //users
     register: `${PATH}register/`,
+    verifyUser: `${PATH}verify_user/`,
+    verifyIp: `${PATH}verify_ip/`,
+    editUserData: `${PATH}change_personal_data/`,
+    forgotPassword: `${PATH}forgot_password/`,
+    resetPassword: `${PATH}reset_password/`,
+    getOAuth2Token: `${PATH}token/`,
+
+    //telegram_bot
+    setTgKey: `${PATH}set_tg_key/`,
+    getTgKey: `${PATH}get_tg_key/`,
+
+    //feedback
+    addFeedback: `${PATH}give_feedback/`,
+    getFeedbacks: `${PATH}get_feedbacks/`,
+
+    //notifications
+    setTgNot: `${PATH}set_tg_notification/`,
+    setEmailNot: `${PATH}set_email_notification/`,
+    getNotStatus: `${PATH}get_notification_status/`,
+
+    //exchange
+    setGateAuthData: `${PATH}set_gate_auth_data/`,
+    setKucoinAuthData: `${PATH}set_kucoin_auth_data/`,
+    setMexcAuthData: `${PATH}set_mexc_auth_data/`,
+    setHuobiAuthData: `${PATH}set_huobi_auth_data/`,
+    getExchangeBalances: `${PATH}get_exchanges_balances/`,
+
+    //bots
+    createBot: `${PATH}create_bot/`,
+    getBots: `${PATH}get_bots/`,
+    announceStats: `${PATH}announce_stats/`
 }
 
 export default endpoints;
